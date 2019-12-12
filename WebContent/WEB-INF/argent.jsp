84abb5822f7a7d266d3927802af96e9ef00de13f
zak39 <fotia.baptiste@hotmail.com>
2019-12-12 23:35:38 +0100

    I changed argent.jsp and MoneyWeb.java file to respond to exercice

--- WebContent/WEB-INF/argent.jsp
+++ WebContent/WEB-INF/argent.jsp
@@ -10,87 +10,43 @@
 		<title>MoneyWeb</title>
 	</head>
 	<body>
-		<p> <c:out value="<p>Bonjour Money Money from JSTL</p>"/></p>
-		<p> <c:out value="J'ai ${ monGrisbis.getfAmount()  } ${ monGrisbis.getfCurrency() } from JSTL"/></p>
-		
-		<p> <c:out value="J'ai ${ monGrisbis02.getfAmount()  } ${ monGrisbis02.getfCurrency() } from JSTL" default="\"Touche pas au Grisbis SALOPE !!!\"" escapeXml="false"/></p>
-		
-		<c:set var="pseudoPage" value="zak39Page01" scope="page"/>
-		<p><c:out value="${ pseudoPage }"></c:out></p>
-		<c:set var="pseudoPage" value="zak39Page02" scope="page"/>
-		<p><c:out value="${ pseudoPage }"></c:out></p>
-		
-		<c:set var="pseudoRequest" value="zak39Request" scope="request"/>
-		<c:set var="pseudoSession" value="zak39Session" scope="session"/>
-		<c:set var="pseudoApplication" value="zak39Application" scope="application"/>
-		
 		<form method="post" action="argentContent">
 			<p>
-				<label for="login">Login :</label>
-				<input type="text" id="login" name="login" />
-				<input type="submit">
+				<label for="amoun01t">Amount :</label>
+				<input type="text" id="amount01" name="amount01" />
+				<label for="currency01">Currency :</label>
+				<select id="currency01" name="currency01"  size="1">
+					<option>EUR</option>
+					<option>USD</option>
+					<option>GBP</option>
+					<option>CHF</option>
+				</select>
 			</p>
 			
-			<p>
-				<label for="pass">Mot de passe :</label>
-				<input type="password" id="pass" name="pass" />
-				<input type="submit">
+						<p>
+				<label for="amount02">Amount :</label>
+				<input type="text" id="amount02" name="amount02" />
+				<label for="currency02">Currency :</label>
+				<select id="currency02" name="currency02"  size="1">
+					<option>EUR</option>
+					<option>USD</option>
+					<option>GBP</option>
+					<option>CHF</option>
+				</select>
 			</p>
+			
+			<input type="submit" value="Add">
 		</form>
 		
-		<c:if test="${! empty form.resultat }">
-			<p><c:out value="${form.resultat}"></c:out></p>
+		<c:if test="${! empty poche }">			
+			<p>
+				<c:forEach items="${poche.getPortefeuil()}" var="money" begin="0" end="100">
+					<p><c:out value="${ money.getfAmount() } ${ money.getfCurrency() }"></c:out></p>	
+				</c:forEach>
+			</p>
+			
 		</c:if>
-		
-		<p>Bonjour Money Money !</p>
-		<p>Vive le capitalisme !!!</p>
-		<p><%
-			String variable = (String) request.getAttribute("variable");
-			out.println(variable);
-		%></p>
-	
-		<p>Je suis en ${ !empty currency ? currency : '' }</p>
-		
-		<p>
-			J'ai ${ monGrisbis.getfAmount()  } ${ monGrisbis.getfCurrency()  }
-		</p>
-	
-		
-		<c:set target="${ monGrisbis }" property="fAmount" value="350"></c:set>
-		<c:out value="J'ai ${ monGrisbis.getfAmount()  } ${ monGrisbis.getfCurrency()  } from JSTL"></c:out>
-	
-		<c:remove var="peudoPage" scope="page"/> <!-- Permet de supprimer la variable de la memoire -->
-		<c:if test="${50 > 10 }" var="variable" scope="session"> 
-			<p><c:out value="C'est vrai 50 est supérieur à 10 !"></c:out></p>
-		</c:if>
-		
-		<c:choose>
-			<c:when test="${ variable02 }">La variable02 existe !</c:when>
-			<c:when test="${ variable03 }">La variable03 existe !</c:when>
-			<c:otherwise>Il n'y a pas de variableXX !!!</c:otherwise>
-		</c:choose>
-		
-		<c:forEach var="i" begin="0" end="10" step="2">
-		
-		<p>Un message n°<c:out value="${ i }"></c:out>!</p>
-		
-		</c:forEach>
-		
-		<c:forEach items="${titres}" var="titre" begin="0" end="2" varStatus="status">
-			<p>n°<c:out value="${status.index }"></c:out> <c:out value="${titre}"></c:out> </p>	
-		</c:forEach>
-		
-		<c:forTokens var="morceau" items="Un élément/Encore un autre élément/Un dernier pour la route" delims="/ ">
-			<p> <c:out value="${morceau}"></c:out> </p>
-		</c:forTokens>
-		
-		<p>
-	    <%
-        for (int i = 0 ; i < 20 ; i++) {
-            out.println("Une nouvelle ligne !<br />");
-        }
-    	%>
-		</p>
+
 
 	</body>
 </html>
--- src/com/web/money/MoneyWeb.java
+++ src/com/web/money/MoneyWeb.java
@@ -2,6 +2,7 @@ package com.web.money;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.UUID;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -46,45 +47,18 @@ public class MoneyWeb extends HttpServlet {
         out.println("</body>");
         out.println("</html>");*/
 		
-		String msg = "I am Macron !!";
-		request.setAttribute("variable", msg);
+        //Money myMoney01 = new Money();
+        //Money myMoney02 = new Money();
+
+		//MoneyBag poche = new MoneyBag();
 		
-		String currency = request.getParameter("currency");
-		request.setAttribute("currency",currency);
-		
-		Money monGrisbis = new Money();
-		monGrisbis.setfAmount(35);
-		monGrisbis.setfCurrency("EUR");
-		
-		request.setAttribute("monGrisbis",monGrisbis);
+		//String pocheID = UUID.randomUUID().toString();	// piste pour transiter les variables : https://stackoverflow.com/questions/10681229/how-to-send-an-object-from-jsp-to-servlet - add : 			<input type="hidden" name="pocheID" value="${pocheID}"/>
 
-		
-        Money myMoney01 = new Money(15,"EUR");
-        Money myMoney02 = new Money(210,"USD");
-        Money myMoney03 = new Money(1500,"CHF");
-        Money myMoney04 = new Money(150,"GBP");
-
-        System.out.println(myMoney01.getfAmount() + myMoney01.getfCurrency());
-        System.out.println(myMoney02.getfAmount() + myMoney02.getfCurrency());
-        System.out.println(myMoney03.getfAmount() + myMoney03.getfCurrency());
-        System.out.println(myMoney04.getfAmount() + myMoney04.getfCurrency());
-
-        myMoney01 = myMoney01.add(30,"EUR");
-        System.out.println(myMoney01.getfAmount() + myMoney01.getfCurrency());
-
-        myMoney02 = myMoney02.add(50,"USD");
-        System.out.println(myMoney02.getfAmount() + myMoney02.getfCurrency());
-
-        MoneyBag poche = new MoneyBag();
-        poche.add(myMoney01);
-        poche.add(myMoney02);
-        poche.add(myMoney03);
+		//request.getSession().setAttribute(pocheID, poche);
+		//request.setAttribute("pocheID", pocheID);
         
-        request.setAttribute("poche", poche);
         
-        String[] titres = { "Nouvelle incendie", "Une île a été découerte", "Chute du dollard" };
-        
-        request.setAttribute("titres", titres);
+        //request.setAttribute("poche", poche);
 		
 		this.getServletContext().getRequestDispatcher("/WEB-INF/argent.jsp").forward(request, response); // On ne met pas "WebContent" car c'est purement cosmétique pour Eclipse
 	}
@@ -96,66 +70,37 @@ public class MoneyWeb extends HttpServlet {
 		// envoie des donneest
 		//doGet(request, response);
 		
-		//##################################################################
-		//
-		// Je met ce code pour eviter des erreurs.
-		// En effet, après avoir cliquer sur le bouton "Envoyer", 
-		// j'aurais un problème car il manque des objets, etc.
-		//
-		//##################################################################
-		
-		String msg = "I am Macron !!";
-		request.setAttribute("variable", msg);
-		
-		String currency = request.getParameter("currency");
-		request.setAttribute("currency",currency);
-		
-		Money monGrisbis = new Money();
-		monGrisbis.setfAmount(35);
-		monGrisbis.setfCurrency("EUR");
-		
-		request.setAttribute("monGrisbis",monGrisbis);
-
-		
-        Money myMoney01 = new Money(15,"EUR");
-        Money myMoney02 = new Money(210,"USD");
-        Money myMoney03 = new Money(1500,"CHF");
-        Money myMoney04 = new Money(150,"GBP");
-
-        System.out.println(myMoney01.getfAmount() + myMoney01.getfCurrency());
-        System.out.println(myMoney02.getfAmount() + myMoney02.getfCurrency());
-        System.out.println(myMoney03.getfAmount() + myMoney03.getfCurrency());
-        System.out.println(myMoney04.getfAmount() + myMoney04.getfCurrency());
-
-        myMoney01 = myMoney01.add(30,"EUR");
-        System.out.println(myMoney01.getfAmount() + myMoney01.getfCurrency());
-
-        myMoney02 = myMoney02.add(50,"USD");
-        System.out.println(myMoney02.getfAmount() + myMoney02.getfCurrency());
+        //Money myMoney01 = new Money();
+        //Money myMoney02 = new Money();
 
         MoneyBag poche = new MoneyBag();
-        poche.add(myMoney01);
-        poche.add(myMoney02);
-        poche.add(myMoney03);
-        
-        request.setAttribute("poche", poche);
-        
-        String[] titres = { "Nouvelle incendie", "Une île a été découerte", "Chute du dollard" };
-        
-        request.setAttribute("titres", titres);
+        //String pocheID = request.getParameter("pocheID");
 		
-		//##################################################################
-		
-		
-        ConnectionForm form = new ConnectionForm();
+        //MoneyBag poche = (com.web.money.MoneyBag) request.getSession().getAttribute(pocheID);
         
-        form.verifierIdentifiants(request);
+        //request.getSession().removeAttribute(pocheID);
         
-        request.setAttribute("form", form);
-        
-		String nom = request.getParameter("nom");
+		String strArgent01 = request.getParameter("amount01");
+		String currency01 = request.getParameter("currency01");
 		
-		request.setAttribute("nom", nom);
+		String strArgent02 = request.getParameter("amount02");
+		String currency02 = request.getParameter("currency02");
+		
+		//MoneyBag currentPoche = (MoneyBag) request.getParameter("currentPoche");
+		
+		if( strArgent01 != "" && currency01 != "")
+		{
+			int argent01 = Integer.parseInt(strArgent01);
+			poche.add(new Money(argent01, currency01));
+		}
+		
+		if( strArgent02 != "" && currency02 != "")
+		{
+			int argent02 = Integer.parseInt(strArgent02);
+			poche.add(new Money(argent02, currency02));
+		}
+		
+        request.setAttribute("poche",poche);
 		
 		this.getServletContext().getRequestDispatcher("/WEB-INF/argent.jsp").forward(request, response); // On ne met pas "WebContent" car c'est purement cosmétique pour Eclipse
 
